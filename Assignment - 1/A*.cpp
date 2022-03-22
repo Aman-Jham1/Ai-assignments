07f3880051a18afc3b18b6cbe88f29b4995e63af
Aman Jham <ajham6222@gmail.com>
2022-03-22 07:40:22 +0530

    Made folder for assignment

---
+++ Assignment - 1/A*.cpp
@@ -0,0 +1,226 @@
+/*
+    AI Assignment-1.
+    Aman Jham - 2019A7PS0071H
+    R Vedang - 2019A7PS0150
+    Shubh - 2019A7PS0100
+*/
+
+/*
+    Problem statement :
+    In 2021, a Mars rover, Perseverance, landed on the moon carrying along with it the first of its kind
+    helicopter named Ingenuity. The goal of the rover was to travel to the center of a crater named Jezero
+    with the help of Ingenuity. Since no help from Earth is available, Perseverance is solely dependent on
+    Ingenuity’s inputs. Ingenuity flies across a couple of meters and gives an aerial topography as an input to
+    Perseverance (FIG A) and based on these inputs the rover takes its next step. Design a communication
+    channel between Ingenuity and Perseverance where the copter sends the expanded states from the
+    initial position of the rover, the rover sends back the decision it takes on the expanded states and so on.
+    Ensure that an optimal heuristic approach search (A*) is used by the rover to decide upon which state
+    to choose. The edges in the topography represent the distances between each check point.
+*/
+
+/*
+    Assumptions or considering foloowing facts for input and output.
+    Number of nodes will be given - nodes
+    Number of edges are - m which are form u, v, w -> meaning undirdceted edge from u to v with wei w
+    start - start state
+    goal - goal state
+    Heursitics value - straight line distance which will be given as an input.
+*/
+
+/*
+    TEST CASE : 
+    14 17
+    0 13
+    0 1 120
+    1 2 113
+    2 3 72
+    3 4 77
+    4 5 122
+    5 6 126
+    6 11 140
+    11 13 105
+    6 12 148
+    12 11 99
+    12 9 82
+    0 9 142
+    0 7 77
+    7 8 71
+    8 9 122
+    9 10 111
+    10 13
+    213
+    550 550 450 510 325 415 235 455 400 325 240 170 205 0
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+#define MAX_DIST 1000000000
+#define all(x) x.begin(), x.end()
+#define sz(x) static_cast<int32_t>(x.size())
+
+vector<int> heuristics_sld;
+vector<int> g;
+
+struct cmp
+{
+    bool operator()(int a, int b) const
+    {
+        return g[a] + heuristics_sld[a] < g[b] + heuristics_sld[b];
+    }
+};
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int nodes, m;
+    int start, goal;
+    cin >> nodes >> m;
+    cin >> start >> goal;
+    vector<vector<pair<int, int> > > graph(nodes);
+    for (int i = 0; i < m; ++i)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        graph[u].push_back(make_pair(v, w));
+        graph[v].push_back(make_pair(u, w));
+    }
+    heuristics_sld.resize(nodes);
+    for (int i = 0; i < nodes; ++i)
+    {
+        cin >> heuristics_sld[i];
+    }
+    vector<int> parents(nodes);
+
+    // auto cmp = [](int a, int b) {
+    //   return g[a] + heuristics_sld[a] < g[b] + heuristics_sld[b];
+    // };
+
+    //set <int, decltype(cmp)> open_list;
+    set<int, cmp> open_list;
+    set<int> closed_list;
+    parents[start] = start;
+    g.assign(nodes, 1e9);
+    g[start] = 0;
+    open_list.insert(start);
+
+    // while (!open_list.empty()) {
+    //   int n = -1;
+    //   for (auto v : open_list) {
+    //       if (n == -1 or g[v] + heuristics_sld[v] < g[n] + heuristics_sld[n]) {
+    //           n = v;
+    //       }
+    //   }
+    //   if (n == -1) {
+    //       cout << "NO PATH EXISTS\n";
+    //       return 0;
+    //   }
+    //   if (n == goal) {
+    //       cout << "SOLUTION FOUND : \n";
+    //       cout << "TOTAL DISTANCE : " << g[n] << '\n';
+    //       cout << "PATH : ";
+    //       vector <int> path;
+    //       while (parents[n] != n) {
+    //           path.push_back(n);
+    //           n = parents[n];
+    //       }
+    //       path.push_back(start);
+    // reverse(all(path));
+    // int cnt = 0;
+    // for (auto &x : path) {
+    //      cout << x << " ";
+    //      if (cnt++ < path.size() - 1) cout << "-> ";
+    // }
+    // cout << '\n';
+    // return 0;
+    //   }
+    //   for (auto &[v, w] : graph[n]) {
+    //       if (!open_list.count(v) and !closed_list.count(v)) {
+    //           g[v] = g[n] + w;
+    //           parents[v] = n;
+    //           open_list.insert(v);
+    //       } else {
+    //           if (g[v] > g[n] + w) {
+    //               g[v] = g[n] + w;
+    //               parents[v] = n;
+    //               if (closed_list.count(v)) {
+    //                   closed_list.erase(v);
+    //                   open_list.insert(v);
+    //               }
+    //           }
+    //       }
+    //   }
+    //   open_list.erase(n);
+    //   closed_list.insert(n);
+    // }
+
+    /*
+    Optimised Version, using compartors for open_list
+    We dont have to iterate in open_list to get current best value.
+    so we can get best in O(logn).
+  */
+
+    while (!open_list.empty())
+    {
+        int n = *open_list.begin();
+        if (n == goal)
+        {
+            cout << "SOLUTION FOUND : \n";
+            cout << "TOTAL DISTANCE : " << g[n] << '\n';
+            cout << "PATH : ";
+            vector<int> path;
+            while (parents[n] != n)
+            {
+                path.push_back(n);
+                n = parents[n];
+            }
+            path.push_back(start);
+            reverse(all(path));
+            int cnt = 0;
+            for (auto &x : path)
+            {
+                cout << x << " ";
+                if (cnt++ < path.size() - 1)
+                    cout << "-> ";
+            }
+            cout << '\n';
+            return 0;
+        }
+        for (auto &[v, w] : graph[n])
+        {
+            if (!open_list.count(v) and !closed_list.count(v))
+            {
+                g[v] = g[n] + w;
+                parents[v] = n;
+                open_list.insert(v);
+            }
+            else
+            {
+                if (g[v] > g[n] + w)
+                {
+                    if (open_list.count(v))
+                        open_list.erase(v);
+                    g[v] = g[n] + w;
+                    parents[v] = n;
+                    open_list.insert(v);
+                    if (closed_list.count(v))
+                    {
+                        closed_list.erase(v);
+                        open_list.insert(v);
+                    }
+                }
+            }
+        }
+        open_list.erase(n);
+        closed_list.insert(n);
+    }
+
+    /*
+    NO PATH FOUND, if found alerady returned.
+  */
+
+    cout << "NO PATH FOUND\n";
+
+    return 0;
+}
--- Assignment - 1/Steepest-hill-climbing.cpp
+++ Assignment - 1/Steepest-hill-climbing.cpp
@@ -25,25 +25,31 @@ using namespace std;
 #define all(x) x.begin(), x.end()
 #define sz(x) static_cast<int32_t>(x.size())
 
-class Board {
+class Board
+{
 private:
-    vector <int> board;
+    vector<int> board;
+
 public:
-    Board() {
+    Board()
+    {
         this->board.resize(12);
     }
     // Board(const Board &other) {
     //     this->board.resize(12);
     //     this->board = other.get_board();
     // }
-    void set_board(vector <int> board) {
+    void set_board(vector<int> board)
+    {
         this->board = board;
     }
-    vector <int> get_board() {
+    vector<int> get_board()
+    {
         return this->board;
     }
-    vector <int> operate(int sector_no, int type) {
-        vector <int> new_board(12);
+    vector<int> operate(int sector_no, int type)
+    {
+        vector<int> new_board(12);
         new_board = board;
         /*
             OPeration accordingly.
@@ -51,7 +57,8 @@ public:
         //this->board = new_board;
         return board;
     }
-    Board clockwise(vector <int> a, int x) {
+    Board clockwise(vector<int> a, int x)
+    {
         Board new_board;
         int val = a[x * 4 + 3];
         a[4 * x + 3] = a[x * 4 + 2];
@@ -60,8 +67,9 @@ public:
         a[x * 4 + 0] = val;
         new_board.set_board(a);
         return new_board;
-    }   
-    Board anti_clockwise(vector <int> a, int x) {
+    }
+    Board anti_clockwise(vector<int> a, int x)
+    {
         Board new_board;
         int val = a[x * 4 + 0];
         a[4 * x + 0] = a[x * 4 + 1];
@@ -71,14 +79,16 @@ public:
         new_board.set_board(a);
         return new_board;
     }
-    Board shift_down(vector<int> a, int x) {
+    Board shift_down(vector<int> a, int x)
+    {
         Board new_board;
         swap(a[x], a[4 + x]);
         new_board.set_board(a);
         return new_board;
     }
-    vector <Board> get_all_states() {
-        vector <Board> nextstates;
+    vector<Board> get_all_states()
+    {
+        vector<Board> nextstates;
         nextstates.push_back(clockwise(this->board, 0));
         nextstates.push_back(clockwise(this->board, 1));
         nextstates.push_back(clockwise(this->board, 2));
@@ -98,33 +108,38 @@ public:
 
         return nextstates;
     }
-    int heuristic_val(Board goalstate) {
+    int heuristic_val(Board &goalstate)
+    {
         int heuristic = 0;
-        vector <int> goalboard = goalstate.get_board();
-        for (int i = 0; i < 12; ++i) {
-            heuristic += this->board[i] == goalboard[i];
+        vector<int> goalboard = goalstate.get_board();
+        for (int i = 0; i < 12; ++i)
+        {
+            heuristic += this->board[i] != goalboard[i];
         }
         return heuristic;
     }
-    void print() {
-        for (auto x : this->board) {
+    void print()
+    {
+        for (auto x : this->board)
+        {
             cout << x << " ";
         }
         cout << '\n';
     }
-    bool is_better(Board other, Board goal) {
-        return this->heuristic_val(goal) > other.heuristic_val(goal); 
+    bool is_better(Board &other, Board &goal)
+    {
+        return this->heuristic_val(goal) < other.heuristic_val(goal);
     }
-    bool operator == (Board other) {
+    bool operator==(Board &other)
+    {
         return this->board == other.get_board();
     }
-    bool eaquals(Board other, Board goal) {
-        return this->heuristic_val(goal) == other.heuristic_val(goal);
-    }
-    void operator = (Board other) {
+    void operator=(Board &other)
+    {
         this->board = other.get_board();
     }
-    bool operator != (Board other) {
+    bool operator!=(Board &other)
+    {
         return this->board != other.get_board();
     }
 };
@@ -141,84 +156,77 @@ public:
 //     return val;
 // }
 
-void print(vector <Board> path) {
-    for (int i = 0; i < path.size(); ++i) {
+void print(vector<Board> path)
+{
+    for (int i = 0; i < path.size(); ++i)
+    {
         cout << "State " << i << " : ";
         path[i].print();
     }
     return;
 }
 
-int32_t main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0); 
-  Board startstate;
-  vector <int> vals(12);
-  for (int i = 0; i < 12; ++i) {
-    cin >> vals[i];
-  }
-  startstate.set_board(vals);
-  Board goalstate;
-  for (int i = 0; i < 12; ++i) {
-    cin >> vals[i];
-  }
-  goalstate.set_board(vals);
-  vector <Board> path;
-  if (startstate == goalstate) {
-    cout << "Start state is goalstate state, 0 steps nedded.\n";
-    return 0;
-  }
-  // def steepest_ascent_hill_climbing(maze):
-  //   currentstate = maze.startstate
-  //   goalstate = maze.goalstate
-  //   path = [currentstate]
-  //   SUCC = currentstate
-  //   while currentstate != goalstate:
-  //       prevstate = currentstate
-  //       for state in maze.nextstate(currentstate):
-  //           if state == goalstate:
-  //               path.append(state)
-  //               return path, SOLUTION_FOUND
-  //           SUCC = state
-  //           if maze.is_better(SUCC, currentstate):
-  //               currentstate = SUCC
-  //       if currentstate == prevstate:
-  //           return path, SOLUTION_NOT_FOUND
-  //       path.append(currentstate)
-  //   return path, SOLUTION_FOUND
-  Board currentstate = startstate;
-  path.push_back(startstate);
-  Board SUCC = currentstate;
-  //cout << goalstate.heuristic_val(goalstate) << '\n';
-  while (true) {
-    Board prevstate = currentstate;
-    vector <Board> nextstates = currentstate.get_all_states();
-    //cout << sz(nextstates) << '\n';
-    for (auto &state : nextstates) {
-        if (state == goalstate) {
-            path.push_back(state);
-            cout << "SOLUTION FOUND\n";
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    Board startstate;
+    vector<int> vals(12);
+    for (int i = 0; i < 12; ++i)
+    {
+        cin >> vals[i];
+    }
+    startstate.set_board(vals);
+    Board goalstate;
+    for (int i = 0; i < 12; ++i)
+    {
+        cin >> vals[i];
+    }
+    goalstate.set_board(vals);
+    vector<Board> path;
+    if (startstate == goalstate)
+    {
+        cout << "Start state is goalstate state, 0 steps nedded.\n";
+        return 0;
+    }
+
+    Board currentstate = startstate;
+    path.push_back(startstate);
+    Board SUCC = currentstate;
+    //cout << goalstate.heuristic_val(goalstate) << '\n';
+    while (currentstate != goalstate)
+    {
+        Board prevstate = currentstate;
+        vector<Board> nextstates = currentstate.get_all_states();
+        //cout << sz(nextstates) << '\n';
+        for (auto &state : nextstates)
+        {
+            if (state == goalstate)
+            {
+                path.push_back(state);
+                cout << "SOLUTION FOUND\n";
+                print(path);
+                return 0;
+            }
+            //SUCC = state;
+            if (state.is_better(SUCC, goalstate))
+            {
+                SUCC = state;
+            }
+        }
+        if (SUCC.is_better(currentstate, goalstate))
+        {
+            currentstate = SUCC;
+        }
+        if (currentstate == prevstate)
+        {
+            cout << "NO SOLUTION FOUND\n";
             print(path);
             return 0;
         }
-        //SUCC = state;
-        if (state.is_better(SUCC, goalstate)) {
-            SUCC = state;
-        }
+        path.push_back(currentstate);
     }
-    if (SUCC.is_better(currentstate, goalstate)) {
-        currentstate = SUCC;
-    }
-    if (currentstate.eaquals(prevstate, goalstate)) {
-        cout << "NO SOLUTION FOUND\n";
-        cout << "MAi yaha pe hu!!!\n";
-        print(path);
-        return 0;
-    }
-    path.push_back(currentstate);
-  }
-  cout << "SOLUTION FOUND\n";
-  print(path);
-  return 0;
+    cout << "SOLUTION FOUND\n";
+    print(path);
+    return 0;
 }
-
